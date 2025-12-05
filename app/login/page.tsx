b609b48fac15131cf8d00571d47c3ae29232d85d
Bruno Matumoto <brunoxd.matumoto@gmail.com>
2025-12-05 12:37:02 -0300

    fix: Suspense in LoginForm

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,17 +4,17 @@ import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
-    <Suspense>
-      <main className="flex items-center justify-center md:h-screen">
-        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-          <div className="flex h-20 w-full items-end rounded-lg bg-(--custom-yellow) p-3 md:h-24">
-            <div className="w-32 text-white md:w-36">
-              <Logo />
-            </div>
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-(--custom-yellow) p-3 md:h-24">
+          <div className="w-32 text-white md:w-36">
+            <Logo />
           </div>
-          <LoginForm />
         </div>
-      </main>
-    </Suspense>
+        <Suspense>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
   );
 }
